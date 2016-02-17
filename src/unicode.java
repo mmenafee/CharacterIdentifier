a23ad9d328af96a25f8fcdf56c50108d802ed024
mmenafee <mmenafee@deerfield.edu>
2016-02-16 21:54:07 -0500

    add array code to if

--- src/unicode.java
+++ src/unicode.java
@@ -26,21 +26,22 @@ public class unicode {
 	
 	public static void fetch() throws Exception {
 		
-		System.out.println("method ran");
 		
-	    BufferedReader br = new BufferedReader(new FileReader("entityfacts.csv"));
-	    String line = null;
+		
+		 BufferedReader br = new BufferedReader(new FileReader("entityfacts.csv"));
+		    String line = null;
 	    
-	    Scanner user_input = new Scanner (System.in);
-		String character;
-		character = user_input.next();
+	    Scanner user_input = new Scanner (Sam.textfield1.getText());
+		String character = user_input.next();
+		
+		
 		System.out.println(character);
 		int characterhex = character.charAt(0);
 		Integer.toHexString(characterhex);
-		final String hexadec = "0x00" + Integer.toHexString(characterhex);
-		//int number = Integer.parseInt(characterhexadecimal);
-		System.out.println(characterhex);
-		System.out.println(hexadec);
+		String hexadec = "0x00" + Integer.toHexString(characterhex);
+		
+		
+		
 		
 	    while ((line = br.readLine()) != null) {
 	      // System.out.println(line);
@@ -52,27 +53,29 @@ public class unicode {
 	    	  //System.out.println(values[1]);
 	      	if(hexadec.equals(values[1]))
 	      	{
-	      		System.out.println("Found Descirption");
 	      		values[4] = values[4].replaceAll("\\s",""); 
 	      		System.out.println(values[4]);
-	      		System.out.println("Found Alpha Entity");
+	      		Sam.descrip.setText("Character Desciption" + values[4]);
 	      		values[3] = values[3].replaceAll("\\s",""); 
 	      		System.out.println(values[3]);
+	      		Sam.aentities.setText("Alpha Entity:" + values[3]);
 	      		
 	      	}
 	      	
 	      }
 	
-				String character1 = Sam.textfield1.getText();
+			
     			Sam.bigtext.setText(Sam.textfield1.getText());
 	    	    Sam.bigtext.setFont(new Font("Verdana", Font.BOLD, 30));
 	    	        
-    			int unicode = character1.charAt(0);
+    			int unicode = character.charAt(0);
     			String hex = Integer.toHexString(unicode);
-    			Sam.textArea1.setText("Decimal: "+ String.valueOf(unicode));
-    			Sam.textArea2.setText(hexadec);
-    			Sam.aentities.setText(values[3]);
-    			Sam.descrip.setText(values[4]);
+    			Sam.textArea1.setText("Decimal:" + String.valueOf(unicode));
+    			Sam.textArea2.setText("Hex:" + hexadec);
+    			Sam.dentities.setText("Decimal Entity: &#" +  String.valueOf(unicode));
+    			Sam.hentities.setText("Hex Entity: &#"+ hexadec);
+    			Sam.javac.setText("Java Encoding: "+unicode);
+    			
    		}
      
 	    }
